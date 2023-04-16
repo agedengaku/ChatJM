41b588e02ba208a878cd58c62dc60b30b1bfc9d9
josetaylor <josemigueltaylor@gmail.com>
2023-04-16 21:44:36 +0900

    Initial commit

---
+++ chatgpt.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+api_key = os.getenv('CHATGPT_API_KEY')
+url = 'https://api.openai.com/v1/chat/completions'
+
+prompt = 'Hello, ChatGPT!'
+payload = {
+    'model': 'gpt-3.5-turbo',
+    'messages': [{'role': 'user', 'content': prompt}],
+    'max_tokens': 10,
+    'temperature': 0.5,
+}
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {api_key}',
+}
+
+response = requests.post(url, json=payload, headers=headers)
+response_data = response.json()
+
+generated_text = response_data
+print(generated_text)

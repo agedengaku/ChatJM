2e7c8a1756ef7609b15fb75686bccd7d6cc0c6e1
josetaylor <josemigueltaylor@gmail.com>
2023-04-16 23:13:41 +0900

    Use openai package

--- chatgpt.py
+++ chatgpt.py
@@ -1,27 +1,34 @@
 from dotenv import load_dotenv
 import os
-import requests
+import openai
 
 load_dotenv()
 
-api_key = os.getenv('CHATGPT_API_KEY')
+openai.api_key = os.getenv('CHATGPT_API_KEY')
+
 url = 'https://api.openai.com/v1/chat/completions'
 
-prompt = 'Hello, ChatGPT!'
-payload = {
-    'model': 'gpt-3.5-turbo',
-    'messages': [{'role': 'user', 'content': prompt}],
-    'max_tokens': 10,
-    'temperature': 0.5,
-}
+prompt = 'Which driver won the 1984 F1 world championship?'
+model = 'gpt-3.5-turbo'
 
-headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f'Bearer {api_key}',
-}
+response = openai.ChatCompletion.create(
+  model=model,
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+  ]
+)
 
-response = requests.post(url, json=payload, headers=headers)
-response_data = response.json()
+answer = response['choices'][0]['message']['content']
+total_tokens_used = response['usage']['total_tokens']
+gpt35turbo_rate_per_1000_tokens = 0.002
+gpt35turbo_rate_per_token = gpt35turbo_rate_per_1000_tokens / 1000
+cost_in_usd = total_tokens_used * gpt35turbo_rate_per_token
+usd_to_jpy_rate = 134
+cost_in_yen = cost_in_usd * usd_to_jpy_rate
 
-generated_text = response_data['choices'][0]['message']['content']
-print(generated_text)
+print(response)
+print(answer)
+print(total_tokens_used)
+print(cost_in_usd)
+print(cost_in_yen)

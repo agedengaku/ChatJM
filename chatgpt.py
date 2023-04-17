19bbbcd7c9a8328b29e438bd85f1f86313e38eb2
josetaylor <josemigueltaylor@gmail.com>
2023-04-17 21:15:53 +0900

    Get question from user via terminal

--- chatgpt.py
+++ chatgpt.py
@@ -5,17 +5,15 @@ import openai
 load_dotenv()
 
 openai.api_key = os.getenv('CHATGPT_API_KEY')
-
 url = 'https://api.openai.com/v1/chat/completions'
-
-prompt = 'Which driver won the 1984 F1 world championship?'
 model = 'gpt-3.5-turbo'
+
+question = input("Ask a question... ")
 
 response = openai.ChatCompletion.create(
   model=model,
   messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
+    {"role": "user", "content": question}
   ]
 )
 
@@ -27,8 +25,5 @@ cost_in_usd = total_tokens_used * gpt35turbo_rate_per_token
 usd_to_jpy_rate = 134
 cost_in_yen = cost_in_usd * usd_to_jpy_rate
 
-print(response)
 print(answer)
-print(total_tokens_used)
-print(cost_in_usd)
-print(cost_in_yen)
+print(f"This answer costs ¥{cost_in_yen}.")

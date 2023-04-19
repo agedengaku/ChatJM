fe9e7fc5e606c08376178476ebe15ce572845f70
agedengaku <jmotaylor2@gmail.com>
2023-04-19 21:36:08 +0900

    Remove the need for getch, and allow to quit by typing the word quit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from chat import ask_question, create_message, insert_message
 from token_cost_calculation import calculate_cost
-import getch
 
-continue_chat = True
 messages=[{"role": "system", "content": "Answer as concisely as possible, unless otherwise instructed."}]
 
-while continue_chat == True:
-  question = input("\nAsk a question: ")
+while True:
+  question = input('\nType a question.\n(Quit anytime by typing "quit" and hitting enter)\n\n')
+
+  if question.strip() == "quit":
+    break
+
   insert_message(messages, create_message("user", question))
   response = ask_question(messages)
 
@@ -14,17 +16,7 @@ while continue_chat == True:
   total_tokens_used = response['usage']['total_tokens']
 
   print(f"\n{answer}\n")
-  print(f"This answer costs ¥{calculate_cost(total_tokens_used)}.\n")
-  
-  print("Continue? (Press y or n)")
-  user_input = getch.getch()
-
-  while user_input not in ["y", "n"]:
-    print("\nInvalid input. Please enter y or n.")
-    user_input = getch.getch()
-
-  if user_input == "n":
-    continue_chat = False
+  print(f"This answer costs ¥{calculate_cost(total_tokens_used)}.")
 
   insert_message(messages, create_message("assistant", answer))
 
